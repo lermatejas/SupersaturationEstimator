144001a274bb3e5bf4ea8280ac4aad4364987ad6
Humberto Morales <131622133+lermatejas@users.noreply.github.com>
2023-07-27 12:21:11 -0400

    CrystalizationData_load uploaded

---
+++ CrystalizationData_load.m
@@ -0,0 +1,9 @@
+% Data from Crystallization Process May 10, 2018.  Urbano Noris sugar mill
+% where Data_r is the number of samples; Nivel_r is level (%); Fcw_r is condenste water  flow rate (m3/s);  TVapor_r is Temperature of vapor (C); 
+% Vacio_r is vaccum preasure (mmHg); Bx_ml_r is the brix of mother liquor(brix);
+% T_r is is Temperature of magma (C);  Bx_mg_r  is the brix of moagma (brix)
+% SS_r is supersaturation;   CC_r is content of crystals;  P_mg_r: Purity of mother liquor (%); 
+% P_ml_r: is Purity of magma(%); Feed_r is Feed Flow Rate (m3/s)
+
+  load CrystalizationData_1 Datos_r Nivel_r Fcw_r TVapor_r Vacio_r Bx_ml_r T_r  Bx_mg_r SS_r  CC_r   P_mg_r P_ml_r Feed_r
+% load CrystalizationData_2 Datos_r Nivel_r Fcw_r TVapor_r Vacio_r Bx_ml_r T_r  Bx_mg_r SS_r  CC_r   P_mg_r P_ml_r Feed_r
